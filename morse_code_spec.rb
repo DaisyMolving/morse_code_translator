9064eb0a65f5ce4dc35c5f08e066acbb9b13aa7b
Daisy Molving <daisymolving@gmail.com>
2016-02-20 13:29:29 +0000

    wrote another spec to test input_check, that if an integer is inputted, method will return false. test fails

--- morse_code_spec.rb
+++ morse_code_spec.rb
@@ -6,4 +6,10 @@ describe MorseCode do
 	 result = morse.input_check("hello")
 	 expect(result).to eq true
   end
+
+  it 'should not take an integer input' do
+	 morse = MorseCode.new
+	 result = morse.input_check(6)
+	 expect(result).to eq false
+  end
 end
