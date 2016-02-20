c59d843a6025a1b0574b949ec6c51e07f16ed86b
Daisy Molving <daisymolving@gmail.com>
2016-02-20 13:38:24 +0000

    wrote spec to test a method called split_input, where if the input is a string, the string is split into letters and returned in an array. test fails

--- morse_code.rb
+++ morse_code.rb
@@ -7,4 +7,5 @@ class MorseCode
 		false
 	 end
   end
+
 end
--- morse_code_spec.rb
+++ morse_code_spec.rb
@@ -12,4 +12,10 @@ describe MorseCode do
 	 result = morse.input_check(6)
 	 expect(result).to eq false
   end
+  
+  it 'should split a string input into letters and return them in an array' do
+	 morse = MorseCode.new
+	 result = morse.split_input("hello")
+	 expect(result).to eq %w(h e l l o)
+  end
 end
