813e776025c20773301b33efb31dace4664ff15a
Daisy Molving <daisymolving@gmail.com>
2016-02-20 14:52:14 +0000

    wrote letters_to_morse that it takes each split letter within letters array and compares it to the keys in morse_pairs hash and returns the value of that key if it finds a match. one test passes, one test fails because i wrote the wrong morse in the spec (i think)

--- morse_code.rb
+++ morse_code.rb
@@ -19,7 +19,19 @@ class MorseCode
   end
 
   def letters_to_morse(input)
-	 %w(.... ..)
+	 morse_dots = []
+	 morse_pairs = {"a" => ".-", "b" => "-..." , "c" => "-.-.", "d" => "-..", "e" => ".", "f" => "..-.", "g" => "--.", "h" => "....", "i" => "..", "j" => ".---", "k" => "-.-", "l" => ".-..", "m" => "--", "n" => "-.", "o" => "---", "p" => ".--.", "q" => "--.-", "r" => ".-.", "s" => "...", "t" => "-", "u" => "..-", "v" => "...-", "w" => ".--", "x" => "-..-", "y" => "-.--", "z" => "--.."}
+
+	 letters = split_input(input)
+	 letters.each do |letter|
+		morse_pairs.each do |alpha, dots|
+		  if alpha == letter
+			 changed_letter = morse_pairs[alpha]
+			 morse_dots << changed_letter
+		  end
+		end
+	 end
+	 morse_dots
   end
 
 end
